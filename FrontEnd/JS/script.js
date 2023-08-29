3563f7342f625037c7fba98a4fbcdbd0b51a3d17
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-08-29 09:10:12 +0200

    "use fetch and add element"

--- FrontEnd/JS/script.js
+++ FrontEnd/JS/script.js
@@ -1,2 +1,11 @@
 fetch ('http://localhost:5678/api/works')
-thenz
+    .then (r=> console.log(r))
+    .then (r=> listworks.json)
+
+const listworks = works[0];
+
+const imageWorks = document.createElement("img");
+imageWorks.src = listworks.image;
+
+const textWorks = document.createElement("p");
+textWorks.innerHTML = listworks.text;
