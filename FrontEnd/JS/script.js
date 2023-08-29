5902b208c12b780646cf48a96174081cba377f29
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-08-29 14:20:29 +0200

    "add element and fix errors"

--- FrontEnd/JS/script.js
+++ FrontEnd/JS/script.js
@@ -1,11 +1,31 @@
-fetch ('http://localhost:5678/api/works')
-    .then (r=> console.log(r))
-    .then (r=> listworks.json)
+const url = fetch ('http://localhost:5678/api/works')
 
-const listworks = works[0];
+fetch ('http://localhost:5678/api/works')
+    .then (response => console.log(response))
+    .then (response => ProjectWorks(works))
+
+let works = 0;
 
 const imageWorks = document.createElement("img");
-imageWorks.src = listworks.image;
+imageWorks.src = works.imageUrl;
 
 const textWorks = document.createElement("p");
-textWorks.innerHTML = listworks.text;
+textWorks.innerHTML = works.title;
+
+/***Child***/
+
+const sectionWorks = document.querySelector(".gallery")
+sectionWorks.appendChild(imageWorks);
+sectionWorks.appendChild(textWorks);
+
+function ProjectWorks(works)
+    {
+        if (response.ok === true)
+        {
+            (console.log('http://localhost:5678/api/works/imageUrl'))
+        }
+        else 
+        {
+            (console.log("erreur"))
+        }
+    }
