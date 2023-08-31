62aaca027ebbf7ec621328af629cec356b6a3bbe
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-08-31 10:21:21 +0200

    "generate first project"

--- FrontEnd/JS/script.js
+++ FrontEnd/JS/script.js
@@ -1,6 +1,26 @@
 const urlFetch = fetch ('http://localhost:5678/api/works');
 let allProject = [];
 
+let i =0;
+
+function galleryCreate(data)
+{
+    for (let i = 0; i < data.length ; i ++); 
+    {  
+        const worksIndex = data[i];
+
+        const imageWorks = document.createElement("img");
+        imageWorks.src = worksIndex.imageUrl;
+
+        const textWorks = document.createElement("p");
+        textWorks.innerText = worksIndex.title;
+
+        const gallery = document.querySelector(".gallery");
+        gallery.appendChild(imageWorks);
+        gallery.appendChild(textWorks);
+    }
+}
+
 async function urlgenerate()
 {
   await urlFetch
@@ -25,26 +45,5 @@ async function urlgenerate()
     )
 }
 
-function galleryCreate(data)
-{
-    for (let i = 0; i < data.length ; i ++); 
-    {  
-        const worksIndex = data[i];
-
-        const imageWorks = document.createElement("img");
-        imageWorks.src = worksIndex.imageUrl;
-
-        const textWorks = document.createElement("p");
-        textWorks.innerText = worksIndex.title;
-
-        const gallery = document.querySelector(".gallery");
-        gallery.appendChild(imageWorks);
-        gallery.appendChild(textWorks);
-    }
-}
-
-async function galleryGenerate ()
-{
-  await galleryCreate();
-}
+urlgenerate();
 
