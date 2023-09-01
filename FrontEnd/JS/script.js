06ac2dc8d000e93242e9ca08b048a187ffbd6627
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-09-01 15:03:11 +0200

    "add filters"

--- FrontEnd/JS/script.js
+++ FrontEnd/JS/script.js
@@ -1,6 +1,5 @@
 const urlFetch = fetch ('http://localhost:5678/api/works');
 
-
 function galleryCreate(data)
 {
     for (let i = 0; i < data.length ; i ++) 
@@ -10,7 +9,7 @@ function galleryCreate(data)
         const gallery = document.querySelector(".gallery");
 
         const divProject = document.createElement("figure");
-        divProject.innerHTML = worksIndex.div;
+        divProject.innerHTML = gallery.div;
 
         const imageWorks = document.createElement("img");
         imageWorks.src = worksIndex.imageUrl;
@@ -42,10 +41,38 @@ async function urlgenerate()
     .catch ((error) => 
     {
       alert ('Error')
-      console.log(error)
+      console.log('error')
     }
     )
 }
 
 urlgenerate();
 
+/***Add category and filters***/
+ const categoryFetch = fetch ("http://localhost:5678/api/categories");
+
+async function categoryGenerate()
+ {
+    await categoryFetch
+    .then ((response) =>
+    {
+      if (response.ok) return (response.json())
+    })
+    .then ((data) =>
+    {
+    console.log(data)
+    })
+    .catch ((error) =>
+    {
+    alert ('Error')
+    console.log('error')
+    });
+ }
+ 
+ const filters = document.querySelector(".filters");
+
+ const btnFilterObject = document.createElement(".btn FilterObject");
+ btnFilterObject.innerHTML = filters.btn;
+
+ filters.appendChild(btnFilterObject);
+ 
