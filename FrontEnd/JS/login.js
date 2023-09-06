3947500fe17f6e64064f6b2a604f81fa16c2d2b8
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-09-06 16:10:45 +0200

    "add function on login form"

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -1,1 +1,42 @@
-const login = login;
+async function getCorrectUser(correctEmail,correctPassword)
+{
+    let correctUser = 
+    {
+        email: correctEmail,
+        password: correctPassword
+    };
+    const response = await fetch("http://localhost:5678/users/login",
+    {
+        method:"post",
+        headers: {
+            "Accept":"application/json",
+            'Content-Type':"application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    return await response.json();
+}
+
+const loginForm = document.querySelector("form");
+let email = document.getElementById("#email");
+let password = document.getElementById("#password");
+const validButton = document.querySelector("submit");
+
+validButton.addEventListener("click", function(event)
+{
+    event.defaultPrevented();
+    let userMail = mail.value;
+    let userPassword = password.value;
+    let data = getCorrectUser(userMail,userPassword);
+
+    if (data != null)
+    {
+        console.log("ok")
+        console.log(data);
+    }
+    else
+    {
+        console.log("erreur")
+        alert("Erreur dans l'identification")
+    }
+})
