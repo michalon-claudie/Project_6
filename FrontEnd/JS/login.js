2d008757da4ac5a9f48990960aee9a06976dc98b
Claudie.Michalon <michalon.claudie@outlook.fr>
2023-09-12 11:33:43 +0200

    "fix error connection"

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -1,21 +1,23 @@
 async function connectUser (email,password){
     const response = await fetch ("http://localhost:5678/api/users/login",{
         method : "POST",
-        headers:
-        {"Content-Type":"application/json"},
-        body : JSON.stringify({
-            email,
-            password
-        })
-    }
+        headers: {"Content-Type":"application/json"},
+        body : JSON.stringify
+            ({
+                email,
+                password
+            })
+        }
     )
-    const responseData = response.json()
+    const responseData = await response.json()
     if (responseData.token) {
-        localStorage.setItem('token', responseData.token);
-        window.location.href = "./index.html";
+        localStorage.setItem('token', responseData.token)
+        window.location.href = "./index.html"
+        console.log("ok")
     } else {
-        console.error("Erreur");
-        alert("Erreur")
+        console.error("L'identifiant ou le mot de passe sont incorrectes")
+        const error = document.querySelector(".error-message")
+        error.innerHTML = "Les informations renseignées sont incorrectes"
     }
 }
 
@@ -23,7 +25,6 @@ const form = document.querySelector(".login")
 const email = document.querySelector('input[type="email"]')
 const pword = document.querySelector('input[type="password"]')
 const validButton = document.querySelector('input[type="submit"]')
-
 const emailValue = email.value
 const passwordValue = pword.value
 
@@ -35,4 +36,4 @@ validButton.addEventListener("click", function(event)
     console.log(emailValue)
     console.log(passwordValue)
     connectUser(emailValue,passwordValue)
-    })
+})
